12a2d456d25463370ffa5bcf63d2d31e8b87ec06
Александр Никишкин <mapuaptu@gmail.com>
2024-04-02 15:37:22 +0300

    Added ToggleTerm

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -15,6 +15,22 @@ if true then
       },
     },
     {
+      "akinsho/toggleterm.nvim",
+      config = true,
+      cmd = "ToggleTerm",
+      keys = { { "<C-\\>", mode = {"n", "t"}, "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+      opts = {
+        open_mapping = [[<C-\\>]],
+        direction = "float",
+        shade_filetypes = {},
+        hide_numbers = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        start_in_insert = true,
+        close_on_exit = true,
+      },
+    },
+    {
       "rcarriga/nvim-notify",
       opts = {
         top_down = false,
@@ -63,28 +79,6 @@ if true then
         },
       },
     },
-    -- {
-    --   "nvim-neorg/neorg",
-    --   ft = "norg",
-    --   build = ":Neorg sync-parsers",
-    --   dependencies = { "nvim-lua/plenary.nvim" },
-    --   config = function()
-    --     require("neorg").setup {
-    --       load = {
-    --         ["core.defaults"] = {}, -- Loads default behaviour
-    --         ["core.concealer"] = {}, -- Adds pretty icons to your documents
-    --         ["core.dirman"] = { -- Manages Neorg workspaces
-    --           config = {
-    --             workspaces = {
-    --               x = "~/projects/my/x/",
-    --             },
-    --             default_workspace = "x"
-    --           },
-    --         },
-    --       },
-    --     }
-    --   end,
-    -- },
   }
 end
 
