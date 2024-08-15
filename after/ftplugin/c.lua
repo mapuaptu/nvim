355ce79ab00ecb71bd9534c8527b7d1ba16dc3f2
Aleksandr Nikishkin <mapuaptu@gmail.com>
2024-08-15 17:47:31 +0300

    Fix current directory for c files

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,5 +1,6 @@
 local anchor = vim.fs.find({ "Makefile" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
 
-local root_dir = vim.fs.dirname(anchor)
+local cwd = vim.fn.getcwd()
+local root_dir = vim.fs.dirname(anchor) or cwd
 
 vim.bo.makeprg = "cd " .. root_dir .. " && make"
