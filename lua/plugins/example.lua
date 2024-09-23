7dd37df8367b27a2839a743790b1cec990af2ec2
Aleksandr Nikishkin <mapuaptu@gmail.com>
2024-09-23 22:55:25 +0300

    Change leetcode.nvim order initialization to prevent inlay_hints rewriting

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,25 @@
 if true then
   return {
     {
+      "kawre/leetcode.nvim",
+      cmd = "Leet",
+      build = ":TSUpdate html",
+      -- dependencies = {
+      --   "nvim-telescope/telescope.nvim",
+      --   "nvim-lua/plenary.nvim", -- required by telescope
+      --   "MunifTanjim/nui.nvim",
+      --
+      --   -- optional
+      --   "nvim-treesitter/nvim-treesitter",
+      --   "rcarriga/nvim-notify",
+      --   "nvim-tree/nvim-web-devicons",
+      -- },
+      opts = {
+        lang = "javascript",
+        -- configuration goes here
+      },
+    },
+    {
       "neovim/nvim-lspconfig",
       opts = {
         inlay_hints = {
@@ -21,25 +40,6 @@ if true then
               },
             },
           },
-        },
-      },
-      {
-        "kawre/leetcode.nvim",
-        cmd = 'Leet',
-        build = ":TSUpdate html",
-        dependencies = {
-          "nvim-telescope/telescope.nvim",
-          "nvim-lua/plenary.nvim", -- required by telescope
-          "MunifTanjim/nui.nvim",
-
-          -- optional
-          "nvim-treesitter/nvim-treesitter",
-          "rcarriga/nvim-notify",
-          "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-          lang = "javascript"
-          -- configuration goes here
         },
       },
     },
