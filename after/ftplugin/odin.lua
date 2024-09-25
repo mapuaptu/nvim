2440e1edbed1b91ecaee2adaa60d76ad24484eac
Aleksandr Nikishkin <mapuaptu@gmail.com>
2024-09-25 16:35:43 +0300

    Changed mkprg for odin

--- after/ftplugin/odin.lua
+++ after/ftplugin/odin.lua
@@ -1,5 +1,4 @@
 local anchor = vim.fs.find({ "main.odin" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
-local cwd = vim.fn.getcwd()
-local root_dir = vim.fs.dirname(anchor) or cwd
+local root_dir = vim.fs.dirname(anchor)
 
-vim.bo.makeprg = "cd " .. root_dir .. " && odin run % -file -o:none "
+vim.bo.makeprg = "cd " .. root_dir .. " && odin run . -o:none "
