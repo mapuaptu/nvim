d5ea1bb59dc3d87cc19f96a9ece652878a43df36
Aleksandr Nikishkin <mapuaptu@gmail.com>
2024-10-05 01:48:50 +0300

    Added outline plugin

--- after/ftplugin/odin.lua
+++ after/ftplugin/odin.lua
@@ -1,4 +1,4 @@
-local anchor = vim.fs.find({ "main.odin" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
+local anchor = vim.fs.find({ "main.odin", ".gitignore" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
 local root_dir = vim.fs.dirname(anchor)
 
-vim.bo.makeprg = "cd " .. root_dir .. " && odin run . -o:none "
+vim.bo.makeprg = "cd " .. root_dir .. " && odin run . -o:none -out:main"
