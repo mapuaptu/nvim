e7e0812c40d93d235004de38df797a6ae446fbc4
Aleksandr Nikishkin <mapuaptu@gmail.com>
2024-12-09 13:26:34 +0200

    Fix

--- after/ftplugin/odin.lua
+++ after/ftplugin/odin.lua
@@ -1,4 +1,4 @@
 local anchor = vim.fs.find({ "main.odin", ".gitignore" }, { upward = true, path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) })[1]
 local root_dir = vim.fs.dirname(anchor)
 
-vim.bo.makeprg = "cd " .. root_dir .. " && odin run . -o:none -out:main"
+vim.bo.makeprg = "cd " .. root_dir .. " && odin run . -vet -o:none -out:main"
