6b8f2feae278ba0ac63b05e4720301ad0a56a87b
Aleksandr Nikishkin <mapuaptu@gmail.com>
2025-02-18 03:55:08 +0200

    Added dap setup for c and odin languages

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,59 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local dap = require("dap")
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+}
+
+dap.configurations.odin = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
+  -- {
+  --   name = "Select and attach to process",
+  --   type = "gdb",
+  --   request = "attach",
+  --   program = function()
+  --      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --   end,
+  --   pid = function()
+  --      local name = vim.fn.input('Executable name (filter): ')
+  --      return require("dap.utils").pick_process({ filter = name })
+  --   end,
+  --   cwd = '${workspaceFolder}'
+  -- },
+  -- {
+  --   name = 'Attach to gdbserver :1234',
+  --   type = 'gdb',
+  --   request = 'attach',
+  --   target = 'localhost:1234',
+  --   program = function()
+  --      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --   end,
+  --   cwd = '${workspaceFolder}'
+  -- },
+}
