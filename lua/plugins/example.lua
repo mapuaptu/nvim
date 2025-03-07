4d5b7eb83ea4abbd48e9c22bddce497d79cf16bf
Aleksandr Nikishkin <mapuaptu@gmail.com>
2025-03-07 11:32:38 +0200

    Fixed bigfile linelenght

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -104,13 +104,20 @@ if true then
       opts = {
         bigfile = {
           notify = true, -- show notification when big file detected
-          size = 0.5 * 1024 * 1024, -- 0.5MB
+          size = 1.5 * 1024 * 1024, -- 1.5MB
+          line_length = 100000, -- average line length (useful for minified files)
           -- Enable or disable features when big file detected
           ---@param ctx {buf: number, ft:string}
           setup = function(ctx)
+            if vim.fn.exists(":NoMatchParen") ~= 0 then
+              vim.cmd([[NoMatchParen]])
+            end
+            Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
             vim.b.minianimate_disable = true
             vim.schedule(function()
-              vim.bo[ctx.buf].syntax = ctx.ft
+              if vim.api.nvim_buf_is_valid(ctx.buf) then
+                vim.bo[ctx.buf].syntax = ctx.ft
+              end
             end)
           end,
         },
